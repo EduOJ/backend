ebb00a38e6f9ce21a4232c41d1eb5d1af073d7c4
suntt2019 <sun123t2@163.com>
2020-08-10 21:16:22 +0800

    Added CRUD api for non-admin users

---
+++ app/controller/user.go
@@ -0,0 +1,91 @@
+package controller
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo/v4"
+	"github.com/leoleoasd/EduOJBackend/app/request"
+	"github.com/leoleoasd/EduOJBackend/app/response"
+	"github.com/leoleoasd/EduOJBackend/base"
+	"github.com/leoleoasd/EduOJBackend/base/utils"
+	"github.com/leoleoasd/EduOJBackend/database/models"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+func GetUser(c echo.Context) error {
+
+	user, err, ok := queryUser(c)
+	if !ok {
+		return err
+	}
+	return c.JSON(http.StatusOK, response.GetUserResponse{
+		Message: "SUCCESS",
+		Error:   nil,
+		Data: struct {
+			models.User `json:"user"`
+		}{
+			user,
+		},
+	})
+}
+
+func GetUsers(c echo.Context) error {
+	req := new(request.GetUsersRequest)
+	err, ok := utils.BindAndValidate(req, &c, utils.GetValidUsername("Username"))
+	if !ok {
+		return err
+	}
+	var users []models.User
+	if req.OrderBy != "" {
+		err = base.DB.Where("username like ? and nickname like ?", "%"+req.Username+"%", "%"+req.Nickname+"%").Order(req.OrderBy).Find(&users).Error
+	} else {
+		err = base.DB.Where("username like ? and nickname like ?", "%"+req.Username+"%", "%"+req.Nickname+"%").Find(&users).Error
+	}
+	if err != nil {
+		panic(errors.Wrap(err, "could not query users"))
+	}
+	if req.Offset > len(users) {
+		return c.JSON(http.StatusNotFound, response.ErrorResp("GET_USERS_OFFSET_OUT_OF_BOUNDS", nil))
+	}
+	if req.Limit > 0 && req.Offset+req.Limit < len(users) {
+		users = users[req.Offset : req.Offset+req.Limit]
+	} else {
+		users = users[req.Offset:]
+	}
+	return c.JSON(http.StatusOK, response.GetUsersResponse{
+		Message: "SUCCESS",
+		Error:   nil,
+		Data: struct {
+			Users  []models.User `json:"users"`
+			Limit  int           `json:"limit"`
+			Offset int           `json:"offset"`
+			Prev   string        `json:"prev"`
+			Next   string        `json:"next"`
+		}{
+			users,
+			req.Limit,
+			req.Offset,
+			"",
+			"", //TODO:fill this
+		},
+	})
+}
+
+func queryUser(c echo.Context) (models.User, error, bool) {
+	id := c.Param("id")
+	user := models.User{}
+	err := base.DB.Where("id = ?", id).First(&user).Error
+	if err == gorm.ErrRecordNotFound {
+		err = base.DB.Where("username = ?", id).First(&user).Error
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				return user, c.JSON(http.StatusNotFound, response.ErrorResp("QUERY_USER_WRONG_ID", nil)), false
+			} else {
+				panic(errors.Wrap(err, "could not query username"))
+			}
+		}
+	} else if err != nil {
+		panic(errors.Wrap(err, "could not query id"))
+	}
+	return user, nil, true
+}
--- app/request/user.go
+++ app/request/user.go
@@ -3,11 +3,11 @@ package request
 type GetUserRequest struct {
 }
 
-type GetUsersRequest struct { //min=1 can be removed
-	Username string `json:"username" form:"username" query:"username" validate:"min=1,max=30,printascii"`
-	Nickname string `json:"username" form:"username" query:"username" validate:"min=1,max=30"`
+type GetUsersRequest struct {
+	Username string `json:"username" form:"username" query:"username" validate:"max=30,printascii"`
+	Nickname string `json:"nickname" form:"nickname" query:"nickname" validate:"max=30"`
 
-	Limit  int `json:"limit" form:"limit" query:"limit" validate:"max=100,min=1"`
+	Limit  int `json:"limit" form:"limit" query:"limit" validate:"max=100,min=0"`
 	Offset int `json:"offset" form:"offset" query:"offset" validate:"min=0"`
 
 	OrderBy string `json:"order_by" form:"order_by" query:"order_by"`
