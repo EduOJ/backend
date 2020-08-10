890e5f0a927267d5090052a9057c90348a24041b
Leo Lu <luyuxuanleo@gmail.com>
2020-08-11 00:20:01 +0800

    Rename helper functions. Re-write get users. Fix tests.

--- base/utils/utils.go
+++ base/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"math/rand"
+	"reflect"
 	"unsafe"
 )
 
@@ -32,4 +33,22 @@ func RandStr(n int) string {
 	}
 
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+func Contain(obj interface{}, target interface{}) bool {
+	targetValue := reflect.ValueOf(target)
+	switch reflect.TypeOf(target).Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < targetValue.Len(); i++ {
+			if targetValue.Index(i).Interface() == obj {
+				return true
+			}
+		}
+	case reflect.Map:
+		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+			return true
+		}
+	}
+
+	return false
 }
