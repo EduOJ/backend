6ed96432d0be9dc35c35640dbed3ecb3a1ea9bc9
Leo Lu <luyuxuanleo@gmail.com>
2020-08-13 16:58:20 +0800

    Added CORS middleware

--- app/routes.go
+++ app/routes.go
@@ -3,9 +3,13 @@ package app
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	"github.com/leoleoasd/EduOJBackend/app/controller"
 	adminController "github.com/leoleoasd/EduOJBackend/app/controller/admin"
 	"github.com/leoleoasd/EduOJBackend/app/middleware"
+	"github.com/leoleoasd/EduOJBackend/base/config"
+	"github.com/leoleoasd/EduOJBackend/base/log"
+	"net/http"
 )
 
 func Register(e *echo.Echo) {
@@ -13,6 +17,24 @@ func Register(e *echo.Echo) {
 		v: validator.New(),
 	}
 	e.Use(middleware.Recover)
+	var origins []string
+	if n, err := config.Get("server.origin"); err == nil {
+		for _, v := range n.(*config.SliceNode).S {
+			if vv, ok := v.Value().(string); ok {
+				origins = append(origins, vv)
+			} else {
+				log.Fatal("Illegal origin name" + v.String())
+				panic("Illegal origin name" + v.String())
+			}
+		}
+	} else {
+		log.Fatal("Illegal origin config", err)
+		panic("Illegal origin config")
+	}
+	e.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+		AllowOrigins: origins,
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 
 	api := e.Group("/api", middleware.Authentication)
 
