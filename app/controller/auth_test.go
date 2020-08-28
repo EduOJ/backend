bcb9fe06716013e45417a7fdead77703afb053f9
suntt2019 <sun123t2@163.com>
2020-08-28 10:27:57 +0800

    Fix auth test

--- app/controller/auth_test.go
+++ app/controller/auth_test.go
@@ -1,14 +1,12 @@
 package controller_test
 
 import (
-	"encoding/json"
 	"github.com/leoleoasd/EduOJBackend/app/request"
 	"github.com/leoleoasd/EduOJBackend/app/response"
 	"github.com/leoleoasd/EduOJBackend/base"
 	"github.com/leoleoasd/EduOJBackend/base/utils"
 	"github.com/leoleoasd/EduOJBackend/database/models"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -28,281 +26,296 @@ func (c testClass) GetID() uint {
 func TestLogin(t *testing.T) {
 	t.Parallel()
 	// strip monotonic time
-	t.Run("loginWithoutParams", func(t *testing.T) {
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: "",
-			Password:        "",
-		}))
-		resp := response.Response{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusBadRequest, httpResp.StatusCode)
-		assert.Equal(t, response.Response{
-			Message: "VALIDATION_ERROR",
-			Error: []interface{}{
-				map[string]interface{}{
-					"field":       "UsernameOrEmail",
-					"reason":      "required",
-					"translation": "用户名为必填字段",
-				},
-				map[string]interface{}{
-					"field":       "Password",
-					"reason":      "required",
-					"translation": "密码为必填字段",
-				},
+
+	userTestingWrongPassword := models.User{
+		Username: "test_login_wrong_password",
+		Nickname: "test_login_wrong_password_nick",
+		Email:    "test_login_wrong_password@e.e",
+		Password: utils.HashPassword("test_login_password"),
+		Roles:    []models.UserHasRole{},
+	}
+	assert.Nil(t, base.DB.Create(&userTestingWrongPassword).Error)
+
+	failTests := []failTest{
+		{
+			name:   "WithoutParams",
+			method: "POST",
+			path:   "/api/auth/login",
+			req: request.LoginRequest{
+				UsernameOrEmail: "",
+				Password:        "",
 			},
-			Data: nil,
-		}, resp)
-	})
-	t.Run("loginNotFound", func(t *testing.T) {
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: "test_login_1_not_found",
-			Password:        "test_login_password",
-		}))
-		resp := response.LoginResponse{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusNotFound, httpResp.StatusCode)
-		assert.Equal(t, "WRONG_USERNAME", resp.Message)
-		assert.Equal(t, nil, resp.Error)
-	})
-	t.Run("loginWithUsernameSuccess", func(t *testing.T) {
-		user := models.User{
-			Username: "test_login_1",
-			Nickname: "test_login_1_rand_str",
-			Email:    "test_login_1@mail.com",
-			Password: utils.HashPassword("test_login_password"),
-			Roles:    []models.UserHasRole{},
-		}
-		base.DB.Create(&user)
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: user.Username,
-			Password:        "test_login_password",
-			RememberMe:      false,
-		}))
-		resp := response.LoginResponse{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusOK, httpResp.StatusCode)
-		assert.Equal(t, "SUCCESS", resp.Message)
-		assert.Equal(t, nil, resp.Error)
-		jsonEQ(t, user, resp.Data.User)
-		token, err := utils.GetToken(resp.Data.Token)
-		base.DB.Where("id = ?", user.ID).First(&user)
-		assert.Equal(t, nil, err)
-		token.User.LoadRoles()
-		assert.True(t, user.UpdatedAt.Equal(token.User.UpdatedAt))
-		jsonEQ(t, user, token.User)
-		assert.False(t, token.RememberMe)
-	})
-	t.Run("loginWithUsernameAndRememberMeSuccess", func(t *testing.T) {
-		user := models.User{
-			Username: "test_login_2",
-			Nickname: "test_login_2_rand_str",
-			Email:    "test_login_2@mail.com",
-			Password: utils.HashPassword("test_login_password"),
-			Roles:    []models.UserHasRole{},
-		}
-		base.DB.Create(&user)
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: user.Username,
-			Password:        "test_login_password",
-			RememberMe:      true,
-		}))
-		resp := response.LoginResponse{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusOK, httpResp.StatusCode)
-		assert.Equal(t, "SUCCESS", resp.Message)
-		assert.Equal(t, nil, resp.Error)
-		jsonEQ(t, user, resp.Data.User)
-		token, err := utils.GetToken(resp.Data.Token)
-		base.DB.Where("id = ?", user.ID).First(&user)
-		assert.Equal(t, nil, err)
-		token.User.LoadRoles()
-		assert.True(t, user.UpdatedAt.Equal(token.User.UpdatedAt))
-		jsonEQ(t, user, token.User)
-		assert.True(t, token.RememberMe)
-	})
-	t.Run("loginWithEmailSuccess", func(t *testing.T) {
-		user := models.User{
-			Username: "test_login_3",
-			Nickname: "test_login_3_rand_str",
-			Email:    "test_login_3@mail.com",
-			Password: utils.HashPassword("test_login_password"),
-			Roles:    []models.UserHasRole{},
-		}
-		base.DB.Create(&user)
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: user.Email,
-			Password:        "test_login_password",
-		}))
-		resp := response.LoginResponse{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusOK, httpResp.StatusCode)
-		assert.Equal(t, "SUCCESS", resp.Message)
-		assert.Equal(t, nil, resp.Error)
-		jsonEQ(t, user, resp.Data.User)
-		token, err := utils.GetToken(resp.Data.Token)
-		base.DB.Where("id = ?", user.ID).First(&user)
-		assert.Equal(t, nil, err)
-		token.User.LoadRoles()
-		jsonEQ(t, user, token.User)
-	})
-	t.Run("loginWrongPassword", func(t *testing.T) {
-		user := models.User{
-			Username: "test_login_4",
-			Nickname: "test_login_4_rand_str",
-			Email:    "test_login_4@mail.com",
-			Password: utils.HashPassword("test_login_password"),
-		}
-		base.DB.Create(&user)
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: user.Email,
-			Password:        "WRONG_PASSWORD",
-		}))
-		resp := response.LoginResponse{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusForbidden, httpResp.StatusCode)
-		assert.Equal(t, "WRONG_PASSWORD", resp.Message)
-		assert.Equal(t, nil, resp.Error)
-	})
-	t.Run("loginWithUsernameAndRolesSuccess", func(t *testing.T) {
-		t.Parallel()
-
-		classA := testClass{ID: 1}
-		dummy := "test_class"
-		adminRole := models.Role{
-			Name:   "testLoginAdmin",
-			Target: &dummy,
-		}
-		base.DB.Create(&adminRole)
-		adminRole.AddPermission("all")
-		user := models.User{
-			Username: "test_login_5",
-			Nickname: "test_login_5_rand_str",
-			Email:    "test_login_5@mail.com",
-			Password: utils.HashPassword("test_login_password"),
-			Roles:    []models.UserHasRole{},
-		}
-		base.DB.Create(&user)
-		user.GrantRole(adminRole, classA)
-
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", request.LoginRequest{
-			UsernameOrEmail: user.Username,
-			Password:        "test_login_password",
-			RememberMe:      false,
-		}))
-		assert.Equal(t, http.StatusOK, httpResp.StatusCode)
-
-		tokens := make([]models.Token, 0)
-		base.DB.Model(&user).Related(&tokens)
-		assert.Equal(t, 1, len(tokens))
-
-		respUser := models.User{}
-		base.DB.Model(&tokens[0]).Preload("Roles.Role.Permissions").Related(&respUser)
-		base.DB.First(&user, user.ID)
-		assert.Equal(t, user, respUser)
-		jsonEQ(t, response.LoginResponse{
-			Message: "SUCCESS",
-			Error:   nil,
-			Data: struct {
-				models.User `json:"user"`
-				Token       string `json:"token"`
-			}{
-				respUser,
-				tokens[0].Token,
+			reqOptions: nil,
+			statusCode: http.StatusBadRequest,
+			resp: response.Response{
+				Message: "VALIDATION_ERROR",
+				Error: []interface{}{
+					map[string]interface{}{
+						"field":       "UsernameOrEmail",
+						"reason":      "required",
+						"translation": "用户名为必填字段",
+					},
+					map[string]interface{}{
+						"field":       "Password",
+						"reason":      "required",
+						"translation": "密码为必填字段",
+					},
+				},
+				Data: nil,
 			},
-		}, httpResp)
-		assert.False(t, tokens[0].RememberMe)
+		},
+		{
+			name:   "NotFound",
+			method: "POST",
+			path:   "/api/auth/login",
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_1_not_found",
+				Password:        "test_login_password",
+			},
+			reqOptions: nil,
+			statusCode: http.StatusNotFound,
+			resp:       response.ErrorResp("WRONG_USERNAME", nil),
+		},
+		{
+			name:   "WrongPassword",
+			method: "POST",
+			path:   "/api/auth/login",
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_wrong_password",
+				Password:        "test_login_password_wrong",
+			},
+			reqOptions: nil,
+			statusCode: http.StatusForbidden,
+			resp:       response.ErrorResp("WRONG_PASSWORD", nil),
+		},
+	}
+	runFailTests(t, failTests, "Login")
+
+	classA := testClass{ID: 1}
+	dummy := "test_class"
+	adminRole := models.Role{
+		Name:   "testLoginAdmin",
+		Target: &dummy,
+	}
+	base.DB.Create(&adminRole)
+	adminRole.AddPermission("all")
+
+	successTests := []struct {
+		name       string
+		user       models.User
+		req        request.LoginRequest
+		role       models.Role
+		roleTarget models.HasRole
+	}{
+		{
+			name: "SuccessWithUsername",
+			user: models.User{
+				Username: "test_login_1",
+				Nickname: "test_login_1_nick",
+				Email:    "test_login_1@mail.com",
+				Password: utils.HashPassword("test_login_1_pwd"),
+				Roles:    []models.UserHasRole{},
+			},
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_1",
+				Password:        "test_login_1_pwd",
+				RememberMe:      false,
+			},
+			role:       models.Role{},
+			roleTarget: nil,
+		},
+		{
+			name: "SuccessWithEmail",
+			user: models.User{
+				Username: "test_login_2",
+				Nickname: "test_login_2_nick",
+				Email:    "test_login_2@mail.com",
+				Password: utils.HashPassword("test_login_2_pwd"),
+				Roles:    []models.UserHasRole{},
+			},
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_2@mail.com",
+				Password:        "test_login_2_pwd",
+				RememberMe:      false,
+			},
+			role:       models.Role{},
+			roleTarget: nil,
+		},
+		{
+			name: "SuccessWithRememberMe",
+			user: models.User{
+				Username: "test_login_3",
+				Nickname: "test_login_3_nick",
+				Email:    "test_login_3@mail.com",
+				Password: utils.HashPassword("test_login_3_pwd"),
+				Roles:    []models.UserHasRole{},
+			},
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_3",
+				Password:        "test_login_3_pwd",
+				RememberMe:      true,
+			},
+			role:       models.Role{},
+			roleTarget: nil,
+		},
+		{
+			name: "SuccessWithRole",
+			user: models.User{
+				Username: "test_login_4",
+				Nickname: "test_login_4_nick",
+				Email:    "test_login_4@mail.com",
+				Password: utils.HashPassword("test_login_4_pwd"),
+			},
+			req: request.LoginRequest{
+				UsernameOrEmail: "test_login_4",
+				Password:        "test_login_4_pwd",
+				RememberMe:      false,
+			},
+			role:       adminRole,
+			roleTarget: classA,
+		},
+	}
+
+	t.Run("testLoginSuccess", func(t *testing.T) {
+		t.Parallel()
+		for _, test := range successTests {
+			t.Run("testLogin"+test.name, func(t *testing.T) {
+				test := test
+				t.Parallel()
+				assert.Nil(t, base.DB.Create(&test.user).Error)
+				if test.role.ID != 0 {
+					test.user.GrantRole(test.role, test.roleTarget)
+				}
+				httpResp := makeResp(makeReq(t, "POST", "/api/auth/login", test.req))
+				resp := response.LoginResponse{}
+				mustJsonDecode(httpResp, &resp)
+				assert.Equal(t, "SUCCESS", resp.Message)
+				assert.Nil(t, resp.Error)
+				resp.Data.User.LoadRoles() // TODO: fix this
+				jsonEQ(t, test.user, resp.Data.User)
+				token := models.Token{}
+				assert.Nil(t, base.DB.Preload("User").Where("token = ?", resp.Data.Token).First(&token).Error)
+				token.User.LoadRoles()
+				jsonEQ(t, test.user, token.User)
+				assert.Equal(t, test.req.RememberMe, token.RememberMe)
+			})
+		}
 	})
 }
 
 func TestRegister(t *testing.T) {
-	t.Run("registerUserWithoutParams", func(t *testing.T) {
-		t.Parallel()
-		httpResp := makeResp(makeReq(t, "POST", "/api/auth/register", request.RegisterRequest{
-			Username: "",
-			Nickname: "",
-			Email:    "",
-			Password: "",
-		}))
-		resp := response.Response{}
-		mustJsonDecode(httpResp, &resp)
-		assert.Equal(t, http.StatusBadRequest, httpResp.StatusCode)
-		assert.Equal(t, response.Response{
-			Message: "VALIDATION_ERROR",
-			Error: []interface{}{
-				map[string]interface{}{
-					"field":       "Username",
-					"reason":      "required",
-					"translation": "用户名为必填字段",
-				},
-				map[string]interface{}{
-					"field":       "Nickname",
-					"reason":      "required",
-					"translation": "昵称为必填字段",
-				},
-				map[string]interface{}{
-					"field":       "Email",
-					"reason":      "required",
-					"translation": "邮箱为必填字段",
-				},
-				map[string]interface{}{
-					"field":       "Password",
-					"reason":      "required",
-					"translation": "密码为必填字段",
-				},
+
+	dummyUserForConflict := models.User{
+		Username: "test_register_conflict",
+		Nickname: "test_register_conflict_nick",
+		Email:    "test_register_conflict@mail.com",
+		Password: utils.HashPassword("test_register_conflict_pwd"),
+	}
+	assert.Nil(t, base.DB.Create(&dummyUserForConflict).Error)
+
+	failTests := []failTest{
+		{
+			name:   "WithoutParams",
+			method: "POST",
+			path:   "/api/auth/register",
+			req: request.RegisterRequest{
+				Username: "",
+				Nickname: "",
+				Email:    "",
+				Password: "",
 			},
-			Data: nil,
-		}, resp)
-	})
-	t.Run("register_user_success", func(t *testing.T) {
+			reqOptions: nil,
+			statusCode: http.StatusBadRequest,
+			resp: response.Response{
+				Message: "VALIDATION_ERROR",
+				Error: []interface{}{
+					map[string]interface{}{
+						"field":       "Username",
+						"reason":      "required",
+						"translation": "用户名为必填字段",
+					},
+					map[string]interface{}{
+						"field":       "Nickname",
+						"reason":      "required",
+						"translation": "昵称为必填字段",
+					},
+					map[string]interface{}{
+						"field":       "Email",
+						"reason":      "required",
+						"translation": "邮箱为必填字段",
+					},
+					map[string]interface{}{
+						"field":       "Password",
+						"reason":      "required",
+						"translation": "密码为必填字段",
+					},
+				},
+				Data: nil,
+			},
+		},
+		{
+			name:   "ConflictUsername",
+			method: "POST",
+			path:   "/api/auth/register",
+			req: request.RegisterRequest{
+				Username: "test_register_conflict",
+				Nickname: "test_register_1_nick",
+				Email:    "test_register_1@mail.com",
+				Password: "test_register_1_pwd",
+			},
+			reqOptions: nil,
+			statusCode: http.StatusConflict,
+			resp:       response.ErrorResp("CONFLICT_USERNAME", nil),
+		},
+		{
+			name:   "ConflictEmail",
+			method: "POST",
+			path:   "/api/auth/register",
+			req: request.RegisterRequest{
+				Username: "test_register_1",
+				Nickname: "test_register_1_nick",
+				Email:    "test_register_conflict@mail.com",
+				Password: "test_register_1_pwd",
+			},
+			reqOptions: nil,
+			statusCode: http.StatusConflict,
+			resp:       response.ErrorResp("CONFLICT_EMAIL", nil),
+		},
+	}
+
+	runFailTests(t, failTests, "Register")
+
+	t.Run("testRegisterSuccess", func(t *testing.T) {
 		t.Parallel()
-		httpResponse := makeResp(makeReq(t, "POST", "/api/auth/register", request.RegisterRequest{
-			Username: "test_register_user_success_0",
-			Nickname: "test_register_user_success_0",
-			Email:    "test_register_user_success_0@mail.com",
-			Password: "test_register_user_success_0",
+		reqUser := models.User{
+			Username: "test_register_3",
+			Nickname: "test_register_3_nick",
+			Email:    "test_register_3@mail.com",
+			Password: "test_register_3_pwd",
+		}
+		httpResp := makeResp(makeReq(t, "POST", "/api/auth/register", request.RegisterRequest{
+			Username: reqUser.Username,
+			Nickname: reqUser.Nickname,
+			Email:    reqUser.Email,
+			Password: reqUser.Password,
 		}))
-		assert.Equal(t, http.StatusCreated, httpResponse.StatusCode)
+		assert.Equal(t, http.StatusCreated, httpResp.StatusCode)
 		resp := response.RegisterResponse{}
-		respBytes, err := ioutil.ReadAll(httpResponse.Body)
-		assert.Equal(t, nil, err)
-		err = json.Unmarshal(respBytes, &resp)
-		assert.Equal(t, nil, err)
-		user := models.User{}
-		err = base.DB.Where("email = ?", "test_register_user_success_0@mail.com").First(&user).Error
-		assert.Equal(t, nil, err)
+		mustJsonDecode(httpResp, &resp)
+		assert.Equal(t, "SUCCESS", resp.Message)
+		assert.Nil(t, resp.Error)
+		// req == resp
+		assert.Equal(t, reqUser.Username, resp.Data.User.Username)
+		assert.Equal(t, reqUser.Nickname, resp.Data.User.Nickname)
+		assert.Equal(t, reqUser.Email, resp.Data.User.Email)
+		databaseUser := models.User{}
+		assert.Nil(t, base.DB.Where("email = ?", "test_register_3@mail.com").First(&databaseUser).Error)
+		// resp == database
+		jsonEQ(t, resp.Data.User, databaseUser)
 		token := models.Token{}
-		err = base.DB.Where("token = ?", resp.Data.Token).Last(&token).Error
-		assert.Equal(t, nil, err)
-		assert.Equal(t, user.ID, token.UserID)
-		jsonEQ(t, resp.Data.User, user)
-		//TODO: also test if user info == requested info
-
-		resp2 := response.Response{}
-		httpResponse = makeResp(makeReq(t, "POST", "/api/auth/register", request.RegisterRequest{
-			Username: "test_register_user_success_0",
-			Nickname: "test_register_user_success_0",
-			Email:    "test_register_user_success_0@mail.com",
-			Password: "test_register_user_success_0",
-		}))
-		mustJsonDecode(httpResponse, &resp2)
-		assert.Equal(t, http.StatusConflict, httpResponse.StatusCode)
-		assert.Equal(t, response.ErrorResp("CONFLICT_EMAIL", nil), resp2)
-		httpResponse = makeResp(makeReq(t, "POST", "/api/auth/register", request.RegisterRequest{
-			Username: "test_register_user_success_0",
-			Nickname: "test_register_user_success_0",
-			Email:    "test_register_user_success_1@mail.com",
-			Password: "test_register_user_success_0",
-		}))
-		mustJsonDecode(httpResponse, &resp2)
-		assert.Equal(t, http.StatusConflict, httpResponse.StatusCode)
-		assert.Equal(t, response.ErrorResp("CONFLICT_USERNAME", nil), resp2)
-
+		assert.Nil(t, base.DB.Where("token = ?", resp.Data.Token).Last(&token).Error)
+		// token == database
+		assert.Equal(t, databaseUser.ID, token.UserID)
 	})
 }
 
