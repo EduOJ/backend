417bdf59066a858dd5eb1fbc5c53ff899b793911
suntt2019 <sun123t2@163.com>
2020-09-02 09:47:19 +0800

    Add problem migration

--- database/migrate.go
+++ database/migrate.go
@@ -270,6 +270,69 @@ func GetMigration() *gormigrate.Gormigrate {
 				return
 			},
 		},
+		// add problems
+		{
+			ID: "add_problems",
+			Migrate: func(tx *gorm.DB) (err error) {
+
+				type TestCase struct {
+					ID uint `gorm:"primary_key" json:"id"`
+
+					ProblemID uint `sql:"index" json:"problem_id"`
+					Score     uint `json:"score"` // 0 for 平均分配
+
+					InputFileName  string `json:"input_file_name" gorm:"size:255"`
+					OutputFileName string `json:"output_file_name" gorm:"size:255"`
+
+					CreatedAt time.Time  `sql:"index" json:"created_at"`
+					UpdatedAt time.Time  `json:"-"`
+					DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+				}
+
+				type Problem struct {
+					ID                 uint   `gorm:"primary_key" json:"id"`
+					Name               string `sql:"index" json:"name" gorm:"size:255"`
+					Description        string `json:"description"`
+					AttachmentFileName string `json:"attachment_file_name" gorm:"size:255"`
+					Public             bool   `json:"public"`
+					Privacy            bool   `json:"privacy"`
+
+					MemoryLimit        uint   `json:"memory_limit"`                         // Byte
+					TimeLimit          uint   `json:"time_limit"`                           // ms
+					LanguageAllowed    string `json:"language_allowed" gorm:"size:255"`     // E.g.    cpp,c,java,python
+					CompileEnvironment string `json:"compile_environment" gorm:"size:2047"` // E.g.  O2=false
+					CompareScriptID    uint   `json:"compare_script_id"`
+
+					TestCases []TestCase `json:"test_cases"`
+
+					CreatedAt time.Time  `json:"created_at"`
+					UpdatedAt time.Time  `json:"-"`
+					DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+				}
+				err = tx.AutoMigrate(&TestCase{}, &Problem{}).Error
+				if err != nil {
+					return
+				}
+				if reflect.TypeOf(tx.DB().Driver()).String() != "*sqlite3.SQLiteDriver" {
+					err = tx.Model(&TestCase{}).AddForeignKey("problem_id", "problems(id)", "CASCADE", "CASCADE").Error
+					if err != nil {
+						return
+					}
+				}
+				return
+			},
+			Rollback: func(tx *gorm.DB) (err error) {
+				err = tx.DropTable("test_cases").Error
+				if err != nil {
+					return
+				}
+				err = tx.DropTable("problems").Error
+				if err != nil {
+					return
+				}
+				return
+			},
+		},
 	})
 }
 
--- database/models/problem.go
+++ database/models/problem.go
@@ -8,8 +8,8 @@ type TestCase struct {
 	ProblemID uint `sql:"index" json:"problem_id"`
 	Score     uint `json:"score"` // 0 for 平均分配
 
-	InputFileName  string `json:"input_file_name"`
-	OutputFileName string `json:"output_file_name"`
+	InputFileName  string `json:"input_file_name" gorm:"size:255"`
+	OutputFileName string `json:"output_file_name" gorm:"size:255"`
 
 	CreatedAt time.Time  `sql:"index" json:"created_at"`
 	UpdatedAt time.Time  `json:"-"`
@@ -18,16 +18,16 @@ type TestCase struct {
 
 type Problem struct {
 	ID                 uint   `gorm:"primary_key" json:"id"`
-	Name               string `sql:"index" json:"name"`
-	Description        string `gorm:"type:'TEXT'" json:"description"`
-	AttachmentFileName string `json:"attachment_file_name"`
-	Public             bool   `json:"public" gorm:"default:true"`
-	Privacy            bool   `json:"privacy" gorm:"default:true"`
+	Name               string `sql:"index" json:"name" gorm:"size:255"`
+	Description        string `json:"description"`
+	AttachmentFileName string `json:"attachment_file_name" gorm:"size:255"`
+	Public             bool   `json:"public"`
+	Privacy            bool   `json:"privacy"`
 
-	MemoryLimit        uint   `json:"memory_limit"`        // Byte
-	TimeLimit          uint   `json:"time_limt"`           // ms
-	LanguageAllowed    string `json:"language_allowed"`    // E.g.    cpp,c,java,python
-	CompileEnvironment string `json:"compile_environment"` // E.g.  O2=false
+	MemoryLimit        uint   `json:"memory_limit"`                         // Byte
+	TimeLimit          uint   `json:"time_limit"`                           // ms
+	LanguageAllowed    string `json:"language_allowed" gorm:"size:255"`     // E.g.    cpp,c,java,python
+	CompileEnvironment string `json:"compile_environment" gorm:"size:2047"` // E.g.  O2=false
 	CompareScriptID    uint   `json:"compare_script_id"`
 
 	TestCases []TestCase `json:"test_cases"`
