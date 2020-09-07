91f527d6713e36719516a682d78b731a3f5ed51b
suntt2019 <sun123t2@163.com>
2020-09-07 17:48:28 +0800

    Fix problem CRUD(field tags...)

--- app/response/resource/problem.go
+++ app/response/resource/problem.go
@@ -3,52 +3,52 @@ package resource
 import "github.com/leoleoasd/EduOJBackend/database/models"
 
 type TestCaseProfileForAdmin struct {
-	ID uint `gorm:"primary_key" json:"id"`
+	ID uint `json:"id"`
 
-	ProblemID uint `sql:"index" json:"problem_id" gorm:"not null"`
-	Score     uint `json:"score" gorm:"default:0;not null"` // 0 for 平均分配
+	ProblemID uint `sql:"index" json:"problem_id"`
+	Score     uint `json:"score"` // 0 for 平均分配
 
-	InputFileName  string `json:"input_file_name" gorm:"size:255;default:'';not null"`
-	OutputFileName string `json:"output_file_name" gorm:"size:255;default:'';not null"`
+	InputFileName  string `json:"input_file_name"`
+	OutputFileName string `json:"output_file_name"`
 }
 
 type TestCaseProfile struct {
-	ID uint `gorm:"primary_key" json:"id"`
+	ID uint `json:"id"`
 
-	ProblemID uint `sql:"index" json:"problem_id" gorm:"not null"`
-	Score     uint `json:"score" gorm:"default:0;not null"` // 0 for 平均分配
+	ProblemID uint `sql:"index" json:"problem_id"`
+	Score     uint `json:"score"` // 0 for 平均分配
 }
 
 type ProblemProfileForAdmin struct {
-	ID                 uint   `gorm:"primary_key" json:"id"`
-	Name               string `sql:"index" json:"name" gorm:"size:255;default:'';not null"`
-	Description        string `json:"description;default:'';not null"`
-	AttachmentFileName string `json:"attachment_file_name" gorm:"size:255;default:'';not null"`
-	Public             bool   `json:"public" gorm:"default:false;not null"`
-	Privacy            bool   `json:"privacy" gorm:"default:false;not null"`
+	ID                 uint   `json:"id"`
+	Name               string `sql:"index" json:"name"`
+	Description        string `json:"description"`
+	AttachmentFileName string `json:"attachment_file_name"`
+	Public             bool   `json:"public"`
+	Privacy            bool   `json:"privacy"`
 
-	MemoryLimit        uint   `json:"memory_limit" gorm:"default:0;not null"`                   // Byte
-	TimeLimit          uint   `json:"time_limit" gorm:"default:0;not null"`                     // ms
-	LanguageAllowed    string `json:"language_allowed" gorm:"size:255;default:'';not null"`     // E.g.    cpp,c,java,python
-	CompileEnvironment string `json:"compile_environment" gorm:"size:2047;default:'';not null"` // E.g.  O2=false
-	CompareScriptID    uint   `json:"compare_script_id" gorm:"default:0;not null"`
+	MemoryLimit        uint64 `json:"memory_limit"`        // Byte
+	TimeLimit          uint   `json:"time_limit"`          // ms
+	LanguageAllowed    string `json:"language_allowed"`    // E.g.    cpp,c,java,python
+	CompileEnvironment string `json:"compile_environment"` // E.g.  O2=false
+	CompareScriptID    uint   `json:"compare_script_id"`
 
 	TestCases []TestCaseProfileForAdmin `json:"test_cases"`
 }
 
 type ProblemProfile struct {
-	ID                 uint   `gorm:"primary_key" json:"id"`
-	Name               string `sql:"index" json:"name" gorm:"size:255;default:'';not null"`
-	Description        string `json:"description;default:'';not null"`
-	AttachmentFileName string `json:"attachment_file_name" gorm:"size:255;default:'';not null"`
-	Public             bool   `json:"public" gorm:"default:false;not null"`
-	Privacy            bool   `json:"privacy" gorm:"default:false;not null"`
+	ID                 uint   `json:"id"`
+	Name               string `sql:"index" json:"name"`
+	Description        string `json:"description"`
+	AttachmentFileName string `json:"attachment_file_name"`
+	Public             bool   `json:"public"`
+	Privacy            bool   `json:"privacy"`
 
-	MemoryLimit        uint   `json:"memory_limit" gorm:"default:0;not null"`                   // Byte
-	TimeLimit          uint   `json:"time_limit" gorm:"default:0;not null"`                     // ms
-	LanguageAllowed    string `json:"language_allowed" gorm:"size:255;default:'';not null"`     // E.g.    cpp,c,java,python
-	CompileEnvironment string `json:"compile_environment" gorm:"size:2047;default:'';not null"` // E.g.  O2=false
-	CompareScriptID    uint   `json:"compare_script_id" gorm:"default:0;not null"`
+	MemoryLimit        uint64 `json:"memory_limit"`        // Byte
+	TimeLimit          uint   `json:"time_limit"`          // ms
+	LanguageAllowed    string `json:"language_allowed"`    // E.g.    cpp,c,java,python
+	CompileEnvironment string `json:"compile_environment"` // E.g.  O2=false
+	CompareScriptID    uint   `json:"compare_script_id"`
 
 	TestCases []TestCaseProfile `json:"test_cases"`
 }
