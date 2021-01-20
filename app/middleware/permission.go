a7eaabd03e0a01999c74e4775797a0c12d2feb65
Leo Lu <luyuxuanleo@gmail.com>
2021-01-21 02:22:35 +0800

    Re-constructed HasPermission middleware.

--- app/middleware/permission.go
+++ app/middleware/permission.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/leoleoasd/EduOJBackend/app/response"
-	"github.com/leoleoasd/EduOJBackend/base/log"
 	"github.com/leoleoasd/EduOJBackend/database/models"
 	"net/http"
 	"strconv"
@@ -21,40 +20,61 @@ func (h *hasRole) TypeName() string {
 	return h.Name
 }
 
-func HasPermission(perm string, targets ...string) func(next echo.HandlerFunc) echo.HandlerFunc {
+type PermissionOption interface {
+	Check(ctx echo.Context) bool
+}
+
+type UnscopedPermission struct {
+	P string
+}
+
+func (p UnscopedPermission) Check(c echo.Context) bool {
+	u := c.Get("user").(models.User)
+	return u.Can(p.P)
+}
+
+type ScopedPermission struct {
+	P string
+	T string
+}
+
+func (p ScopedPermission) Check(c echo.Context) bool {
+	u := c.Get("user").(models.User)
+	id, err := strconv.ParseUint(c.Param("id"), 10, strconv.IntSize)
+	if err != nil {
+		return false
+	}
+	return u.Can(p.P, &hasRole{
+		ID:   uint(id),
+		Name: p.T,
+	})
+}
+
+type OrPermission struct {
+	A PermissionOption
+	B PermissionOption
+}
+
+func (p OrPermission) Check(c echo.Context) bool {
+	return p.A.Check(c) || p.B.Check(c)
+}
+
+type AndPermission struct {
+	A PermissionOption
+	B PermissionOption
+}
+
+func (p AndPermission) Check(c echo.Context) bool {
+	return p.A.Check(c) && p.B.Check(c)
+}
+
+func HasPermission(p PermissionOption) func(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			_u := c.Get("user")
-			if _u == nil {
-				log.Fatalf("%s don't have a login check middleware!!", c.Path())
-				return response.InternalErrorResp(c)
+			if p.Check(c) {
+				return next(c)
 			}
-			if u, ok := _u.(models.User); ok {
-				can := false
-				if len(targets) == 0 {
-					can = u.Can(perm)
-				} else if len(targets) == 1 {
-					id, err := strconv.ParseUint(c.Param("id"), 10, strconv.IntSize)
-					if err != nil {
-						log.Errorf("illegal id: %s", c.Param("id"))
-						return c.JSON(http.StatusForbidden, response.ErrorResp("PERMISSION_DENIED", nil))
-					}
-					can = u.Can(perm, &hasRole{
-						ID:   uint(id),
-						Name: targets[0],
-					})
-				} else {
-					log.Fatalf("%s registered permission middleware with more than one permission!", c.Path())
-					return response.InternalErrorResp(c)
-				}
-				if can {
-					return next(c)
-				} else {
-					return c.JSON(http.StatusForbidden, response.ErrorResp("PERMISSION_DENIED", nil))
-				}
-			}
-			log.Fatalf("%s's user is not a models.User!", c.Path())
-			return response.InternalErrorResp(c)
+			return c.JSON(http.StatusForbidden, response.ErrorResp("PERMISSION_DENIED", nil))
 		}
 	}
 }
--- app/routes.go
+++ app/routes.go
@@ -29,15 +29,15 @@ func Register(e *echo.Echo) {
 
 	admin := api.Group("/admin", middleware.Logged)
 	admin.POST("/user",
-		controller.AdminCreateUser, middleware.HasPermission("manage_user")).Name = "admin.user.createUser"
+		controller.AdminCreateUser, middleware.HasPermission(middleware.UnscopedPermission{P: "manage_user"})).Name = "admin.user.createUser"
 	admin.PUT("/user/:id",
-		controller.AdminUpdateUser, middleware.HasPermission("manage_user")).Name = "admin.user.updateUser"
+		controller.AdminUpdateUser, middleware.HasPermission(middleware.UnscopedPermission{P: "manage_user"})).Name = "admin.user.updateUser"
 	admin.DELETE("/user/:id",
-		controller.AdminDeleteUser, middleware.HasPermission("manage_user")).Name = "admin.user.deleteUser"
+		controller.AdminDeleteUser, middleware.HasPermission(middleware.UnscopedPermission{P: "manage_user"})).Name = "admin.user.deleteUser"
 	admin.GET("/user/:id",
-		controller.AdminGetUser, middleware.HasPermission("read_user")).Name = "admin.user.getUser"
+		controller.AdminGetUser, middleware.HasPermission(middleware.UnscopedPermission{P: "read_user"})).Name = "admin.user.getUser"
 	admin.GET("/users",
-		controller.AdminGetUsers, middleware.HasPermission("read_user")).Name = "admin.user.getUsers"
+		controller.AdminGetUsers, middleware.HasPermission(middleware.UnscopedPermission{P: "read_user"})).Name = "admin.user.getUsers"
 
 	api.GET("/user/me", controller.GetMe, middleware.Logged).Name = "user.getMe"
 	api.PUT("/user/me", controller.UpdateMe, middleware.Logged).Name = "user.updateMe"
@@ -50,11 +50,11 @@ func Register(e *echo.Echo) {
 	api.POST("/image", controller.CreateImage, middleware.Logged).Name = "image.createImage"
 
 	admin.POST("/problem",
-		controller.CreateProblem, middleware.HasPermission("create_problem")).Name = "problem.createProblem"
+		controller.CreateProblem, middleware.HasPermission(middleware.UnscopedPermission{P: "create_problem"})).Name = "problem.createProblem"
 	admin.PUT("/problem/:id",
-		controller.UpdateProblem, middleware.HasPermission("update_problem", "problem")).Name = "problem.updateProblem"
+		controller.UpdateProblem, middleware.HasPermission(middleware.ScopedPermission{P: "update_problem", T: "problem"})).Name = "problem.updateProblem"
 	admin.DELETE("/problem/:id",
-		controller.DeleteProblem, middleware.HasPermission("delete_problem", "problem")).Name = "problem.deleteProblem"
+		controller.DeleteProblem, middleware.HasPermission(middleware.ScopedPermission{P: "delete_problem", T: "problem"})).Name = "problem.deleteProblem"
 
 	api.GET("/problem/:id", controller.GetProblem).Name = "problem.getProblem"
 	api.GET("/problems", controller.GetProblems).Name = "problem.getProblems"
@@ -63,26 +63,26 @@ func Register(e *echo.Echo) {
 
 	admin.POST("/problem/:id/test_case",
 		controller.CreateTestCase,
-		middleware.HasPermission("update_problem", "problem")).Name = "problem.createTestCase"
+		middleware.HasPermission(middleware.ScopedPermission{P: "update_problem", T: "problem"})).Name = "problem.createTestCase"
 	admin.PUT("/problem/:id/test_case/:test_case_id",
 		controller.UpdateTestCase,
-		middleware.HasPermission("update_problem", "problem")).Name = "problem.updateTestCase"
+		middleware.HasPermission(middleware.ScopedPermission{P: "update_problem", T: "problem"})).Name = "problem.updateTestCase"
 	admin.DELETE("/problem/:id/test_case/all",
 		controller.DeleteTestCases,
-		middleware.HasPermission("update_problem", "problem")).Name = "problem.deleteTestCases"
+		middleware.HasPermission(middleware.ScopedPermission{P: "update_problem", T: "problem"})).Name = "problem.deleteTestCases"
 	admin.DELETE("/problem/:id/test_case/:test_case_id",
 		controller.DeleteTestCase,
-		middleware.HasPermission("update_problem", "problem")).Name = "problem.deleteTestCase"
+		middleware.HasPermission(middleware.ScopedPermission{P: "update_problem", T: "problem"})).Name = "problem.deleteTestCase"
 
 	admin.GET("/problem/:id/test_case/:test_case_id/input_file",
 		controller.GetTestCaseInputFile,
-		middleware.HasPermission("read_problem_secret", "problem")).Name = "problem.getTestCaseInputFile"
+		middleware.HasPermission(middleware.ScopedPermission{P: "read_problem_secret", T: "problem"})).Name = "problem.getTestCaseInputFile"
 	admin.GET("/problem/:id/test_case/:test_case_id/output_file",
 		controller.GetTestCaseOutputFile,
-		middleware.HasPermission("read_problem_secret", "problem")).Name = "problem.getTestCaseOutputFile"
+		middleware.HasPermission(middleware.ScopedPermission{P: "read_problem_secret", T: "problem"})).Name = "problem.getTestCaseOutputFile"
 
 	admin.GET("/logs",
-		controller.AdminGetLogs, middleware.HasPermission("read_logs")).Name = "admin.getLogs"
+		controller.AdminGetLogs, middleware.HasPermission(middleware.UnscopedPermission{P: "read_logs"})).Name = "admin.getLogs"
 
 	if config.MustGet("debug", false).Value().(bool) {
 		log.Debugf("Adding pprof handlers. SHOULD NOT BE USED IN PRODUCTION")
