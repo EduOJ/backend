dfe80e3193d508f849b8ada83a14f7da0663d48b
suntt2019 <sun123t2@163.com>
2021-01-27 00:46:22 +0800

    Add Sample in API CreateTestCase and UpdateTestCase; Update API document

--- app/request/problem.go
+++ app/request/problem.go
@@ -1,5 +1,62 @@
 package request
 
+type CreateProblemRequest struct {
+	Name        string `json:"name" form:"name" query:"name" validate:"required,max=255"`
+	Description string `json:"description" form:"description" query:"description" validate:"required"`
+	// attachment_file(optional)
+	Public  *bool `json:"public" form:"public" query:"public" validate:"required"`
+	Privacy *bool `json:"privacy" form:"privacy" query:"privacy" validate:"required"`
+
+	MemoryLimit        uint64 `json:"memory_limit" form:"memory_limit" query:"memory_limit" validate:"required"`                     // Byte
+	TimeLimit          uint   `json:"time_limit" form:"time_limit" query:"time_limit" validate:"required"`                           // ms
+	LanguageAllowed    string `json:"language_allowed" form:"language_allowed" query:"language_allowed" validate:"required,max=255"` // E.g.    cpp,c,java,python
+	CompileEnvironment string `json:"compile_environment" form:"compile_environment" query:"compile_environment" validate:"max=255"` // E.g.  O2=false
+	CompareScriptID    uint   `json:"compare_script_id" form:"compare_script_id" query:"compare_script_id" validate:"required"`
+}
+
+type UpdateProblemRequest struct {
+	Name        string `json:"name" form:"name" query:"name" validate:"required,max=255"`
+	Description string `json:"description" form:"description" query:"description" validate:"required"`
+	// attachment_file(optional)
+	Public  *bool `json:"public" form:"public" query:"public" validate:"required"`
+	Privacy *bool `json:"privacy" form:"privacy" query:"privacy" validate:"required"`
+
+	MemoryLimit        uint64 `json:"memory_limit" form:"memory_limit" query:"memory_limit" validate:"required"`                     // Byte
+	TimeLimit          uint   `json:"time_limit" form:"time_limit" query:"time_limit" validate:"required"`                           // ms
+	LanguageAllowed    string `json:"language_allowed" form:"language_allowed" query:"language_allowed" validate:"required,max=255"` // E.g.    cpp,c,java,python
+	CompileEnvironment string `json:"compile_environment" form:"compile_environment" query:"compile_environment" validate:"max=255"` // E.g.  O2=false
+	CompareScriptID    uint   `json:"compare_script_id" form:"compare_script_id" query:"compare_script_id" validate:"required"`
+}
+
+type DeleteProblemRequest struct {
+}
+
+type CreateTestCaseRequest struct {
+	Score  uint `json:"score" form:"score" query:"score"` // 0 for 平均分配
+	Sample bool `json:"sample" form:"sample" query:"sample"`
+	// input_file(required)
+	// output_file(required)
+}
+
+type GetTestCaseInputFileRequest struct {
+}
+
+type GetTestCaseOutputFileRequest struct {
+}
+
+type UpdateTestCaseRequest struct {
+	Score  uint `json:"score" form:"score" query:"score"` // 0 for 平均分配
+	Sample bool `json:"sample" form:"sample" query:"sample"`
+	// input_file(optional)
+	// output_file(optional)
+}
+
+type DeleteTestCaseRequest struct {
+}
+
+type DeleteTestCasesRequest struct {
+}
+
 type GetProblemRequest struct {
 }
 
--- app/response/problem.go
+++ app/response/problem.go
@@ -24,3 +24,56 @@ type GetProblemsResponse struct {
 		Next     *string            `json:"next"`
 	} `json:"data"`
 }
+
+type CreateProblemResponse struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		*resource.ProblemForAdmin `json:"problem"`
+	} `json:"data"`
+}
+
+type GetProblemResponseForAdmin struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		*resource.ProblemForAdmin `json:"problem"`
+	} `json:"data"`
+}
+
+type GetProblemsResponseForAdmin struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		Problems []resource.ProblemForAdmin `json:"problems"`
+		Total    int                        `json:"total"`
+		Count    int                        `json:"count"`
+		Offset   int                        `json:"offset"`
+		Prev     *string                    `json:"prev"`
+		Next     *string                    `json:"next"`
+	} `json:"data"`
+}
+
+type UpdateProblemResponse struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		*resource.ProblemForAdmin `json:"problem"`
+	} `json:"data"`
+}
+
+type CreateTestCaseResponse struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		*resource.TestCaseForAdmin `json:"test_case"`
+	} `json:"data"`
+}
+
+type UpdateTestCaseResponse struct {
+	Message string      `json:"message"`
+	Error   interface{} `json:"error"`
+	Data    struct {
+		*resource.TestCaseForAdmin `json:"test_case"`
+	} `json:"data"`
+}
