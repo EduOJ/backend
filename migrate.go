250f32b4283a60f40e3fbe03358dcc22ded7638a
Leo Lu <luyuxuanleo@gmail.com>
2021-02-07 21:51:29 +0800

    Remove code for debug

--- migrate.go
+++ migrate.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/leoleoasd/EduOJBackend/base"
 	"github.com/leoleoasd/EduOJBackend/base/log"
 	"github.com/leoleoasd/EduOJBackend/database"
-	"github.com/leoleoasd/EduOJBackend/database/models"
-	"time"
 )
 
 func doMigrate() {
@@ -51,26 +48,4 @@ rollback_last: rollback last migration.`)
 			}
 		}
 	}
-	p := models.Problem{
-		ID:                 0,
-		Name:               "",
-		Description:        "",
-		AttachmentFileName: "",
-		Public:             false,
-		Privacy:            false,
-		MemoryLimit:        0,
-		TimeLimit:          0,
-		LanguageAllowed:    nil,
-		CompileEnvironment: "",
-		CompareScriptID:    0,
-		TestCases:          nil,
-		CreatedAt:          time.Time{},
-		UpdatedAt:          time.Time{},
-		DeletedAt:          nil,
-	}
-	log.Debug(base.DB.Save(&p).Error)
-	p1 := models.Problem{}
-	log.Debug(base.DB.First(&p1, 1).Error)
-	log.Debug(p1)
-
 }
