248bf52238234ed4d873767898d2e634cce555e3
suntt2019 <sun123t2@163.com>
2021-02-25 22:17:11 +0800

    Fix get submission

--- app/request/submission.go
+++ app/request/submission.go
@@ -9,8 +9,8 @@ type GetSubmissionRequest struct {
 }
 
 type GetSubmissionsRequest struct {
-	ProblemId uint `json:"problem" form:"problem" query:"problem"`
-	UserId    uint `json:"user" form:"user" query:"user"`
+	ProblemId uint `json:"problem_id" form:"problem_id" query:"problem_id"`
+	UserId    uint `json:"user_id" form:"user_id" query:"user_id"`
 
 	Limit  int `json:"limit" form:"limit" query:"limit" validate:"max=100,min=0"`
 	Offset int `json:"offset" form:"offset" query:"offset" validate:"min=0"`
