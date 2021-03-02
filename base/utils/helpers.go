eacf5a8affc6d0e50143fb44dc9329a314868007
Leo Lu <luyuxuanleo@gmail.com>
2021-03-02 14:14:02 +0800

    Don't panic on bad request.

--- base/utils/helpers.go
+++ base/utils/helpers.go
@@ -24,7 +24,7 @@ func PanicIfDBError(db *gorm.DB, message string) {
 func BindAndValidate(req interface{}, c echo.Context) (err error, ok bool) {
 	// TODO: return a HttpError instead of writing to response.
 	if err := c.Bind(req); err != nil {
-		panic(err)
+		return c.JSON(http.StatusBadRequest, response.ErrorResp("BAD_REQUEST_PARAMETER", nil)), false
 	}
 	if err := c.Validate(req); err != nil {
 		if e, ok := err.(validator.ValidationErrors); ok {
