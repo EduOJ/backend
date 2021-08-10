aeeaf07a7adbd0fb5d2a0c9da6c48873bb5f69da
Jingyfgugu <jingyufei1@126.com>
2021-08-10 16:05:39 +0800

    Add EmailVerificationField AddEmailVerificationTable ChangeLoggedMiddleware

--- base/main.go
+++ base/main.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"github.com/duo-labs/webauthn/webauthn"
+	"github.com/go-mail/mail"
 	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
 	"github.com/minio/minio-go/v7"
@@ -13,3 +14,4 @@ var Redis *redis.Client
 var DB *gorm.DB
 var Storage *minio.Client
 var WebAuthn *webauthn.WebAuthn
+var Mail mail.Dialer
