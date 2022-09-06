b74abb1f6bcb0d8aa5b2778bf8c6bc91ca4105bd
Invariant64 <1319438556@qq.com>
2022-09-06 17:40:23 +0800

    backend file

--- app/request/solution.go
+++ app/request/solution.go
@@ -11,4 +11,11 @@ type GetSolutionRequest struct {
 }
 
 type GetSolutionsRequest struct {
+	Search string `json:"search" form:"search" query:"search"`
+	UserID uint   `json:"user_id" form:"user_id" query:"user_id" validate:"min=0,required_with=Tried Passed"`
+
+	Limit  int `json:"limit" form:"limit" query:"limit" validate:"max=100,min=0"`
+	Offset int `json:"offset" form:"offset" query:"offset" validate:"min=0"`
+
+	Tags string `json:"tags" form:"tags" query:"tags"`
 }
